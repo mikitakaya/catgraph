9a1e9116d236082268ac2598c5d283cd68499098
mikitakaya <108921094mikitakaya@users.noreply.github.com>
2022-11-05 13:04:27 +0000

    [Add]usersテーブルを作成

--- db/migrate/20221105125342_devise_create_users.rb
+++ db/migrate/20221105125342_devise_create_users.rb
@@ -32,6 +32,10 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.1]
       # t.string   :unlock_token # Only if unlock strategy is :email or :both
       # t.datetime :locked_at
 
+      # 名前を保存するカラム
+      t.string :name, null: false
+      # ユーザー名を保存するカラム
+      t.string :username, null: false
 
       t.timestamps null: false
     end
