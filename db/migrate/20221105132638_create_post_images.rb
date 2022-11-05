5268fe752eb40d1e90045e634c612f6cf946e10d
mikitakaya <108921094mikitakaya@users.noreply.github.com>
2022-11-05 13:43:56 +0000

    [Add]post_imagesテーブルを作成

--- db/migrate/20221105132638_create_post_images.rb
+++ db/migrate/20221105132638_create_post_images.rb
@@ -1,8 +1,13 @@
 class CreatePostImages < ActiveRecord::Migration[6.1]
   def change
     create_table :post_images do |t|
-
-      t.timestamps
+     # ユーザーIDを保存するカラム
+     t.integer :user_id, null: false
+     # タイトルを保存するカラム
+     t.string :title, null: false
+     # 説明を保存するカラム
+     t.text :body, null: false
+     t.timestamps
     end
   end
 end
