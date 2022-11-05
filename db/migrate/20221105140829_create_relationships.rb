cd31eccd64ffbfab686b1c6629028fd5c3485901
mikitakaya <108921094mikitakaya@users.noreply.github.com>
2022-11-05 14:11:54 +0000

    [Add]relationshipsテーブルを作成

--- db/migrate/20221105140829_create_relationships.rb
+++ db/migrate/20221105140829_create_relationships.rb
@@ -1,7 +1,10 @@
 class CreateRelationships < ActiveRecord::Migration[6.1]
   def change
     create_table :relationships do |t|
-
+      # フォローするユーザーのIDを保存するカラム
+      t.integer :follower_id, null: false
+      # フォローされるユーザーのIDを保存するカラム
+      t.integer :followed_id, null: false
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_11_05_140404) do
+ActiveRecord::Schema.define(version: 2022_11_05_140829) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -70,6 +70,13 @@ ActiveRecord::Schema.define(version: 2022_11_05_140404) do
     t.integer "user_id", null: false
     t.string "title", null: false
     t.text "body", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "relationships", force: :cascade do |t|
+    t.integer "follower_id", null: false
+    t.integer "followed_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
