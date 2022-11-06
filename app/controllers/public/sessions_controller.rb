9cddce738cf618bbdcbd8d99aed5d0ba87e8de00
mikitakaya <108921094mikitakaya@users.noreply.github.com>
2022-11-06 13:39:35 +0000

    [Add]public/sessions_controller.rbにユーザーが退会しているかを判断するメソッドを追加

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -24,4 +24,24 @@ class Public::SessionsController < Devise::SessionsController
   # def configure_sign_in_params
   #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
   # end
+
+  before_action :user_state, only: [:create]
+
+  protected
+  # 退会しているかを判断するメソッド
+  def user_state
+   # 入力されたemailからアカウントを1件取得
+   @user = User.find_by(email: params[:user][:email])
+   # アカウントを取得できなかった場合、このメソッドを終了する
+   return if !@user
+   # 取得したアカウントのパスワードと入力されたパスワードが一致してるかを判別
+   if @user.valid_password?(params[:user][:password])
+    # 取得したアカウントのis_deletedがno_active（true、退会）の場合
+    if @user.is_deleted == "no_active"
+     # 退会済みのため、新規登録画面へリダイレクトする
+     redirect_to new_user_registration_path
+    end
+   end
+  end
+
 end
