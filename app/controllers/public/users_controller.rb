d30e6b6cb536ffe84ed69db082d80b9360975245
mikitakaya <108921094mikitakaya@users.noreply.github.com>
2022-11-10 12:56:07 +0000

    [Add]public/user編集機能を作成中

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -1,18 +1,22 @@
 class Public::UsersController < ApplicationController
 
   def show
-   # 登録済みのデータを取得する
+   # レコードを1件だけ取得
    @user = User.find(params[:id])
   # @userが投稿したpost_imageを全て取得する
    @post_images = @user.post_images
   end
 
   def edit
-   # 登録済みのデータを取得する
+   # レコードを1件だけ取得
    @user = User.find(params[:id])
   end
 
   def update
+   # ログイン中のユーザー
+   @user = current_user
+   @user.update(user_params_update)
+   redirect_to user_path(@user.id)
   end
 
   def unsubscribe
@@ -23,8 +27,8 @@ class Public::UsersController < ApplicationController
 
   private
   # ユーザーデータのストロングパラメータ
-  def user_params
-   params.require(:customer).permit(:name, :username, :introduction, :email, :is_deleted)
+  def user_params_update
+   params.require(:user).permit(:name, :username, :introduction, :email, :is_deleted, :profile_image)
   end
 
 end
