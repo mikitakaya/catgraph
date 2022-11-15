b9b1aa54ef5d0c99d9680ab64200be67128501c0
mikitakaya <108921094mikitakaya@users.noreply.github.com>
2022-11-15 11:20:33 +0000

    [Update]いいね機能を修正

--- app/controllers/public/favorites_controller.rb
+++ app/controllers/public/favorites_controller.rb
@@ -3,19 +3,14 @@ class Public::FavoritesController < ApplicationController
   before_action :post_image_params, only: [:create, :destroy]
 
   def create
-   Favorite.create(user_id: current_user.id, post_image: @post_image.id)
-   # post_image = PostImage.find(params[:post_image_id])
-   # favorite = current_user.favorites.new(post_image_id: post_image.id)
-   favorite.save
-   redirect_to post_image_path(post_image)
+   Favorite.create(user_id: current_user.id, post_image_id: @post_image.id)
+   redirect_to post_image_path(@post_image)
   end
 
   def destroy
    favorite = Favorite.find_by(user_id: current_user.id, post_image_id: @post_image.id)
-   # post_image = PostImage.find(params[:post_image_id])
-   # favorite = current_user.favorites.find_by(post_image_id: post_image.id)
    favorite.destroy
-   redirect_to post_image_path(post_image)
+   redirect_to post_image_path(@post_image)
   end
 
   private
--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -4,5 +4,5 @@ class Favorite < ApplicationRecord
   # favorite（N）はpost_image（1）に属する
   belongs_to :post_image
   # 1つの投稿に対して、1つしかいいねをつけられない
-  validates_uniqueness_of :post_id, scope: :user_id
+  validates_uniqueness_of :post_image_id, scope: :user_id
 end
