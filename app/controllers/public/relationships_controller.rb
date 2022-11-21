79da8d4b157c3f745c1c5be7e7d5c92c6da9776e
mikitakaya <108921094mikitakaya@users.noreply.github.com>
2022-11-21 14:54:24 +0000

    [Update]フォロー・フォロワー一覧画面を作成中

--- app/controllers/public/relationships_controller.rb
+++ app/controllers/public/relationships_controller.rb
@@ -13,8 +13,8 @@ class Public::RelationshipsController < ApplicationController
 
   # フォロー一覧
   def followings
-   user = User.find(params[:user_id])
-   @users = user.followings
+   @user = User.find(params[:user_id])
+   @users = @user.followings
   end
 
   # 　フォロワー一覧
