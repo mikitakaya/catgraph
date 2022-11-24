03628b13c4178c1704c265d36bded823aa3846f7
mikitakaya <108921094mikitakaya@users.noreply.github.com>
2022-11-24 12:08:34 +0000

    [Add]admin/user詳細画面を作成

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,9 +1,12 @@
 class Admin::UsersController < ApplicationController
   def index
-   @users = User.all
+   @users = User.page(params[:page]).per(5)
   end
 
   def show
+   @user = User.find(params[:id])
+   # 1ページあたりの表示件数を「6件」に設定
+   @post_images = @user.post_images.page(params[:page]).per(6)
   end
 
   def edit
