125a69030635f03c6a3722f150b403609d9675cf
mikitakaya <108921094mikitakaya@users.noreply.github.com>
2022-12-21 12:34:50 +0000

    [Update]レイアウトを微調整

--- app/models/user.rb
+++ app/models/user.rb
@@ -27,7 +27,7 @@ class User < ApplicationRecord
   VALID_PASSWORD_REGEX = /\A[!-~]+\z/
   # nameとusernameのデータは存在しなければならない、かつ一意性を持たせる
   # nameは2～20文字の範囲で制限する
-  validates :name, presence: true, uniqueness: true, length: { in: 2..20 }
+  validates :name, presence: true, uniqueness: true, length: { in: 2..8 }
   # usernameは6～20文字の範囲で制限する
   validates :username, presence: true, uniqueness: true, length: { in: 6..20 }, format: { with: VALID_PASSWORD_REGEX }
   # introductionは最大140文字に制限する
