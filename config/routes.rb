146766a425bfffb13ffd8ab382e012d784d3e18c
mikitakaya <108921094mikitakaya@users.noreply.github.com>
2022-12-23 13:09:34 +0000

    [Update]微調整

--- config/routes.rb
+++ config/routes.rb
@@ -1,75 +1,74 @@
 Rails.application.routes.draw do
 
-  # 検索機能
-  get '/search', to: 'searches#search'
+ # 検索機能
+ get '/search', to: 'searches#search'
 
-  # ユーザー用
-  # URL /users/sign_in ...
-  devise_for :users,skip: [:passwords], controllers: {
-    registrations: "public/registrations",
-    sessions: 'public/sessions'
-  }
+ # ユーザー用
+ # URL /users/sign_in ...
+ devise_for :users,skip: [:passwords], controllers: {
+  registrations: "public/registrations",
+  passwords: 'public/passwords',
+  sessions: 'public/sessions'
+ }
 
-  # 管理者用
-  # URL /admin/sign_in ...
-  devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
-    sessions: "admin/sessions"
-  }
-  devise_scope :user do
-    post 'users/guest_sign_in', to: 'public/sessions#guest_sign_in'
-    # TIPS: ユーザー登録失敗のリダイレクトのエラーを防ぐ https://github.com/heartcombo/devise/blob/master/app/controllers/devise/registrations_controller.rb
-    get '/users', to: 'public/registrations#new'
+ # 管理者用
+ # URL /admin/sign_in ...
+ devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
+  sessions: "admin/sessions"
+ }
+
+ devise_scope :user do
+  post 'users/guest_sign_in', to: 'public/sessions#guest_sign_in'
+  # TIPS: ユーザー登録失敗のリダイレクトのエラーを防ぐ https://github.com/heartcombo/devise/blob/master/app/controllers/devise/registrations_controller.rb
+  get '/users', to: 'public/registrations#new'
+ end
+
+ # ユーザー用
+ scope module: :public do
+  # public/homes
+  root to: "homes#top"
+  get 'about' => "homes#about", as: "about"
+
+  # public/users
+  resources :users, only: [:show, :edit, :update] do
+   # いいね一覧
+   collection do
+    get :favorites
+   end
+
+   # public/relationships
+   resource :relationships, only: [:create, :destroy]
+   get 'followings' => 'relationships#followings', as: 'followings'
+   get 'followers' => 'relationships#followers', as: 'followers'
   end
 
-  # ユーザー用
-  scope module: :public do
-    # public/homes
-    root to: "homes#top"
-    get 'about' => "homes#about", as: "about"
+  get 'users/:id/unsubscribe' => "users#unsubscribe", as: "unsubscribe"
+  patch 'users/:id/withdraw' => "users#withdraw", as: "withdraw"
 
-    # public/users
-    resources :users, only: [:show, :edit, :update] do
-      # いいね一覧
-      collection do
-        get :favorites
-      end
+  # public/post_images
+  resources :post_images do
+   # public/favorites
+   resource :favorites, only: [:create, :destroy]
 
-      # public/relationships
-      resource :relationships, only: [:create, :destroy]
-      get 'followings' => 'relationships#followings', as: 'followings'
-      get 'followers' => 'relationships#followers', as: 'followers'
-    end
-    get 'users/:id/unsubscribe' => "users#unsubscribe", as: "unsubscribe"
-    patch 'users/:id/withdraw' => "users#withdraw", as: "withdraw"
-
-    # public/post_images
-    resources :post_images do
-      # public/favorites
-      resource :favorites, only: [:create, :destroy]
-
-      # public/post_comments
-      resources :post_comments, only: [:create, :destroy]
-    end
-
-
+   # public/post_comments
+   resources :post_comments, only: [:create, :destroy]
   end
+ end
 
-  # 管理者用
-  namespace :admin do
-    # admin/homes
-    get '/' => "homes#top", as: "top"
+ # 管理者用
+ namespace :admin do
+  # admin/homes
+  get '/' => "homes#top", as: "top"
 
-    # admin/users
-    resources :users, only: [:index, :show, :edit, :update, :destroy]
+  # admin/users
+  resources :users, only: [:index, :show, :edit, :update, :destroy]
 
-    # admin/post_images
-    resources :post_images, only: [:show, :destroy] do
-     # admin/post_comments
-     resources :post_comments, only: [:destroy]
-    end
-
-
+  # admin/post_images
+  resources :post_images, only: [:show, :destroy] do
+   # admin/post_comments
+   resources :post_comments, only: [:destroy]
   end
+ end
 
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+ # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
